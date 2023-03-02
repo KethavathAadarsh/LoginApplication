11d13552960221b8927cf1cf56c4778046e0870f
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-02 16:44:50 +0530

    added a data

--- LoginApplication/Client.cs
+++ LoginApplication/Client.cs
@@ -7,10 +7,10 @@ namespace LoginApplication
 {
     public class Client
     {
-        public string Return { get; set; }
+        public string ReturnId { get; set; }
         public string Clientname { get; set; }
         public string Clientid { get; set; }
-        public string Reference { get; set; }
+        public string Preparer { get; set; }
         public string Officegroup { get; set; }
         public string Status { get; set; }
         public string Lastactivated { get; set; }
