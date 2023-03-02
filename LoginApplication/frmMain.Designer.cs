d56edb60d4ff84a008e7a9ad0176d02365563bfc
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-02 21:24:15 +0530

    Adding text box

--- LoginApplication/frmMain.Designer.cs
+++ LoginApplication/frmMain.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.btn_LogOut = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FilterbyclientId = new System.Windows.Forms.TextBox();
+            this.GO = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_LogOut
@@ -46,18 +50,43 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(72, 41);
+            this.dataGridView1.Location = new System.Drawing.Point(137, 43);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(531, 284);
+            this.dataGridView1.Size = new System.Drawing.Size(489, 294);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            // 
+            // FilterbyclientId
+            // 
+            this.FilterbyclientId.Location = new System.Drawing.Point(12, 73);
+            this.FilterbyclientId.Name = "FilterbyclientId";
+            this.FilterbyclientId.Size = new System.Drawing.Size(119, 20);
+            this.FilterbyclientId.TabIndex = 2;
+            // 
+            // GO
+            // 
+            this.GO.Location = new System.Drawing.Point(91, 99);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(40, 23);
+            this.GO.TabIndex = 3;
+            this.GO.Text = "GO";
+            this.GO.UseVisualStyleBackColor = true;
+            this.GO.Click += new System.EventHandler(this.GO_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 337);
+            this.Controls.Add(this.GO);
+            this.Controls.Add(this.FilterbyclientId);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_LogOut);
             this.Name = "frmMain";
@@ -66,7 +95,9 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +105,8 @@
 
         private System.Windows.Forms.Button btn_LogOut;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox FilterbyclientId;
+        private System.Windows.Forms.Button GO;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
     }
 }
