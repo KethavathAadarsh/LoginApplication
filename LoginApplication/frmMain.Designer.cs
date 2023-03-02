db7b03b6853201cc0ba630c3096a87caed9a95d8
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-02 16:45:06 +0530

    datagrid added

--- LoginApplication/frmMain.Designer.cs
+++ LoginApplication/frmMain.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.btn_LogOut = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_LogOut
@@ -41,16 +43,29 @@
             this.btn_LogOut.UseVisualStyleBackColor = true;
             this.btn_LogOut.Click += new System.EventHandler(this.btn_LogOut_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(72, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(531, 284);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 337);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_LogOut);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Application";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +73,6 @@
         #endregion
 
         private System.Windows.Forms.Button btn_LogOut;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
