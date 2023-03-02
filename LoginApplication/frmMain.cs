8b4c98101d3c616c989f833e42a3dd9dbb6ea941
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-02 16:44:35 +0530

    added a data

--- LoginApplication/frmMain.cs
+++ LoginApplication/frmMain.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace LoginApplication
 {
     public partial class frmMain : Form
     {
+        public List<Client> People { get; set; }
         public frmMain()
         {
+            People = GetPeople();
             InitializeComponent();
+
         }
+
+        private List<Client> GetPeople()
+        {
+            var list = new List<Client>();
+            list.Add(new Client()
+            {
+                ReturnId = "21c caratworld v1",
+                Clientname = "caratworld inc",
+                Clientid = "caratworld",
+                Preparer = "superglass",
+                Officegroup = "PCLLP",
+                Status = "updated",
+                Lastactivated = "26th jan",
+            });
+            return list;
+        }
+
         //btn_LogOut Click Event
         private void btn_LogOut_Click(object sender, EventArgs e)
         {
@@ -22,5 +43,10 @@ namespace LoginApplication
             Application.Exit();
         }
 
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            var People = this.People;
+            dataGridView1.DataSource = People;
+        }
     }
 }
