521569413f4da05200260b646d235af24886dc61
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-03 23:08:59 +0530

    none to name

--- LoginApplication/frmMain.Designer.cs
+++ LoginApplication/frmMain.Designer.cs
@@ -152,10 +152,10 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(72, 97);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(63, 21);
+            this.radioButton2.Size = new System.Drawing.Size(66, 21);
             this.radioButton2.TabIndex = 9;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "None";
+            this.radioButton2.Text = "Name";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // label7
