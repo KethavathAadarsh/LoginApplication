6262006733b4cc8990f1ba076944119aa59dc582
aadarsh Kethavat <aadarsh@hoc.com>
2023-03-04 00:29:35 +0530

    designer

--- LoginApplication/frmMain.Designer.cs
+++ LoginApplication/frmMain.Designer.cs
@@ -52,7 +52,7 @@
             // 
             // btn_LogOut
             // 
-            this.btn_LogOut.Location = new System.Drawing.Point(328, 54);
+            this.btn_LogOut.Location = new System.Drawing.Point(438, 54);
             this.btn_LogOut.Margin = new System.Windows.Forms.Padding(4);
             this.btn_LogOut.Name = "btn_LogOut";
             this.btn_LogOut.Size = new System.Drawing.Size(129, 28);
@@ -66,19 +66,19 @@
             this.dataGridView1.AllowDrop = true;
             this.dataGridView1.ColumnHeadersHeight = 32;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dataGridView1.Location = new System.Drawing.Point(181, 90);
+            this.dataGridView1.Location = new System.Drawing.Point(278, 90);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1642, 602);
+            this.dataGridView1.Size = new System.Drawing.Size(1703, 321);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // FilterbyclientId
             // 
-            this.FilterbyclientId.Location = new System.Drawing.Point(4, 125);
+            this.FilterbyclientId.Location = new System.Drawing.Point(37, 125);
             this.FilterbyclientId.Margin = new System.Windows.Forms.Padding(4);
             this.FilterbyclientId.Name = "FilterbyclientId";
             this.FilterbyclientId.Size = new System.Drawing.Size(157, 22);
@@ -86,7 +86,7 @@
             // 
             // GO
             // 
-            this.GO.Location = new System.Drawing.Point(4, 153);
+            this.GO.Location = new System.Drawing.Point(141, 155);
             this.GO.Margin = new System.Windows.Forms.Padding(4);
             this.GO.Name = "GO";
             this.GO.Size = new System.Drawing.Size(53, 28);
@@ -102,7 +102,7 @@
             // 
             // printpdf
             // 
-            this.printpdf.Location = new System.Drawing.Point(181, 54);
+            this.printpdf.Location = new System.Drawing.Point(278, 54);
             this.printpdf.Margin = new System.Windows.Forms.Padding(4);
             this.printpdf.Name = "printpdf";
             this.printpdf.Size = new System.Drawing.Size(139, 28);
@@ -114,7 +114,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 225);
+            this.label2.Location = new System.Drawing.Point(81, 215);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 17);
             this.label2.TabIndex = 6;
@@ -123,7 +123,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(38, 273);
+            this.label5.Location = new System.Drawing.Point(85, 262);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 17);
             this.label5.TabIndex = 6;
@@ -132,7 +132,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(53, 320);
+            this.label6.Location = new System.Drawing.Point(95, 309);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(48, 17);
             this.label6.TabIndex = 6;
@@ -141,7 +141,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(2, 97);
+            this.radioButton1.Location = new System.Drawing.Point(37, 97);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(73, 21);
             this.radioButton1.TabIndex = 8;
@@ -152,7 +152,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(72, 97);
+            this.radioButton2.Location = new System.Drawing.Point(128, 97);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(66, 21);
             this.radioButton2.TabIndex = 9;
@@ -165,7 +165,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.SystemColors.Highlight;
             this.label7.ForeColor = System.Drawing.SystemColors.Menu;
-            this.label7.Location = new System.Drawing.Point(18, 77);
+            this.label7.Location = new System.Drawing.Point(53, 77);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(125, 17);
             this.label7.TabIndex = 10;
@@ -176,7 +176,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Highlight;
             this.label1.ForeColor = System.Drawing.SystemColors.Menu;
-            this.label1.Location = new System.Drawing.Point(45, 194);
+            this.label1.Location = new System.Drawing.Point(90, 187);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 12;
@@ -185,7 +185,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(47, 367);
+            this.label8.Location = new System.Drawing.Point(90, 356);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(54, 17);
             this.label8.TabIndex = 11;
@@ -202,7 +202,7 @@
             "form25",
             "form21",
             "itrreturnform25"});
-            this.comboBox1.Location = new System.Drawing.Point(21, 246);
+            this.comboBox1.Location = new System.Drawing.Point(56, 235);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 13;
@@ -211,7 +211,7 @@
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(21, 293);
+            this.comboBox2.Location = new System.Drawing.Point(56, 282);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 14;
@@ -220,7 +220,7 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(21, 340);
+            this.comboBox3.Location = new System.Drawing.Point(56, 329);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 24);
             this.comboBox3.TabIndex = 15;
@@ -229,7 +229,7 @@
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(21, 387);
+            this.comboBox4.Location = new System.Drawing.Point(56, 376);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(121, 24);
             this.comboBox4.TabIndex = 16;
